611a8531d0bd4e80fe375cd8496eedcd9f3f21ff
PenGuinMike <104021074@live.asia.edu.tw>
2016-10-25 23:36:50 +0800

    Update hw02.java

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,24 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int x = scn.nextInt();
+		int sum = 0;
+		int a =1;
+		while(sum<=x){
+			sum+=a;
+			a++;
+			if(sum>x){
+				a=a-2;
+				System.out.print("a="+a);
+			}
+		}
+			
 	}
 
 }
