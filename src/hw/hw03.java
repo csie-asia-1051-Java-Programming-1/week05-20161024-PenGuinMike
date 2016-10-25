6c5ff5672670578c218ab81b85d9b71013ba0028
PenGuinMike <104021074@live.asia.edu.tw>
2016-10-25 23:39:21 +0800

    Update hw03.java

--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -1,10 +1,22 @@
 package hw;
 
+import java.util.*;
+
 public class hw03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn =new Scanner(System.in);
+		int x = scn.nextInt();
+		int a = x+1;
+		int v1;
+		for(int i=1;i>=0;i++){
+			x=x*(a-1);
+			if(x>10000){
+				System.out.println("i="+(i+1));
+				break;
+			}
+		}
 	}
 
 }
