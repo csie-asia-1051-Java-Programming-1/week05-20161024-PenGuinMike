b2600e02d16942945a8e6f2e989928549c4cc91f
PenGuinMike <104021074@live.asia.edu.tw>
2016-10-27 16:12:10 +0800

    finish

--- src/hw/hw05.java
+++ src/hw/hw05.java
@@ -7,16 +7,31 @@ public class hw05 {
 		Scanner scn = new Scanner(System.in);
 		int x = scn.nextInt();
 		int data [][]=new int[x][x];
-		int a =1;
-		for(int i=0;i<x;i++){
-			for(int j=0;j<i;j++){
-				data[j][i]=a;
-				a++;
+		int a = 1,t = 0;
+		while(t<=(x-1)*2){
+			if(t<x){
+				for(int i =t;i>=0;i--){
+					int v1=t-i;
+					data[i][v1]=a;
+					a++;
+				}
 			}
+			if(t>=x){
+				for(int i=t-x+1;i<x;i++){
+					int q = t-i;
+					data[q][i]=a;
+					a++;
+					//System.out.print("t="+t);
+					//System.out.print("q="+q);
+					//System.out.print("i="+i);
+				}
+			}
+			t++;
 		}
+		
 		for(int i=0;i<x;i++){
 			for(int j=0;j<x;j++){
-				System.out.print(data[j][i]);
+				System.out.print(data[i][j]+"\t");
 			}System.out.println();
 		}
 	}
